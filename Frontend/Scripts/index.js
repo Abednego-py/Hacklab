7e690cf953cc1a789724317940f9149b891f4d24
Abednego-py <abednegoaginam@gmail.com>
2022-07-12 22:26:01 +0100

    first commit

--- Frontend/Scripts/index.js
+++ Frontend/Scripts/index.js
@@ -1,10 +1,16 @@
 
+let firstName = document.getElementById('firstname')
+let email =  document.getElementById('email')
+let lastname = document.getElementById('lastname')
+let num =  document.getElementById('number')
+let password =  document.getElementById('password')
+
+
 function signUp(){
     fetch('https://voathclone.vercel.app/api/v1/auth/signup', {
         method : 'POST',
         body : JSON.stringify(),
-        headers: {'content-type':
-        'application/json'
+        headers: {'Content-type': 'application/json; charset=UTF-8'
     }
     })
     .then(
--- Frontend/Scripts/script2.js
+++ Frontend/Scripts/script2.js
@@ -2,7 +2,7 @@
 
 //Code for checking Weather Condition
 let inputval = document.querySelector('#cityinput')
-const btn = document.getElementById('btn')
+const btn = document.getElementById('add')
 let city = document.querySelector('#cityoutput')
 let descrip = document.querySelector('#description')
 let temp = document.querySelector('#temp')
@@ -12,56 +12,42 @@ let humidity = document.querySelector('#humidity')
 
 let apik = "3e2930a606c1c6880391f32a652b3ff0"
 
-//Access the longitude and latitude of a current location
-
 //kelvin to celcious. 1 Kelvin is equal to -272.15 Celsius.
+
 function convertion(val){
     return (val - 273).toFixed(2)
 }
-
-
-    function success(position){
-        let latitude = position.coords.latitude
-         let longitude = position.coords.longitude
-            fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apik}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                let nameval = data['name']
-                let descrip = data['weather']['0']['description']
-                let tempature = data['main']['temp']
-                let wndspd = data['wind']['speed']
-                let hum_ = data['main']['humidity']
-                city.innerHTML=`Weather of <span>${nameval}<span>`
-                temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
-                description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
-                wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
-                humidity.innerHTML = `Humidity: <span>${hum_}`
-            
-        })
-        .catch(err => alert('You entered Wrong city name'))
-      
-        
-        }
-        if(navigator.geolocation){
-            window.navigator.geolocation.getCurrentPosition(success, console.error)
-        }
-
 //Now we have to collect all the information with the help of fetch method
 
+    btn.addEventListener('click', function(){
 
-   
- 
+//This is the api link from where all the information will be collected
 
+        fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
+        .then(res => res.json())
 
-   function geoCode(lat, long){
-    let apiKey = 'c0d57193e59b436686e9668f7ebf55d9'
+         //.then(data => console.log(data))
 
-   fetch('https://api.opencagedata.com/geocode/v1/json'
-   + '?'
-   + 'q=' + encodeURIComponent(lat + long)
-   + '&key=' + apiKey)
-  .then((response) => response.json())
-  .then((data) => console.log());
-   }
+        .then(data => {
+            console.log(data)
 
+//Now you need to collect the necessary information with the API link. Now I will collect that information and store it in different constants.
+
+            let nameval = data['name']
+            let descrip = data['weather']['0']['description']
+            let tempature = data['main']['temp']
+            let wndspd = data['wind']['speed']
+            let humid =  data['main']['humidity']
+//Now with the help of innerHTML you have to make arrangements to display all the information in the webpage.
+            city.innerHTML=`Weather Condition of <span>${nameval}</span>`
+            temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
+            description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
+            wind.innerHTML = `Wind Speed: <span>${wndspd} km/h </span>`
+            humidity.innerHTML = `Humudity : <span>${humid} </span>` 
+
+        })
+
+//Now the condition must be added that what if you do not input anything in the input box.
+        .catch(err => alert('You entered Wrong city name'))
+    })
+//If you click on the submit button without inputting anythi
