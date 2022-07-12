4c8044ec32dc0c2a5befc0bc688fccf3b5b6ee00
Abednego-py <abednegoaginam@gmail.com>
2022-07-12 22:28:19 +0100

    first upload

--- Frontend/Scripts/index.js
+++ Frontend/Scripts/index.js
@@ -1,27 +1,77 @@
 
-let firstName = document.getElementById('firstname')
-let email =  document.getElementById('email')
-let lastname = document.getElementById('lastname')
-let num =  document.getElementById('number')
-let password =  document.getElementById('password')
-
-
-function signUp(){
-    fetch('https://voathclone.vercel.app/api/v1/auth/signup', {
-        method : 'POST',
-        body : JSON.stringify(),
-        headers: {'Content-type': 'application/json; charset=UTF-8'
-    }
-    })
-    .then(
-        res => res.json()
-    )
-    .then(data =>
-         console.log(data))
-    .catch(err => console.log(err))
-}
-
+let btn =  document.getElementById('btn-signUp')
 let sign_up = document.getElementById('signUp')
+
+window.addEventListener('load', ()=>{
+    function sendData() {
+        const XHR = new XMLHttpRequest();
+
+        const FD =  new FormData(sign_up)
+
+        XHR.open( "POST", "https://voathclone.vercel.app/api/v1/auth/signup")
+        XHR.send(FD)
+
+        XHR.addEventListener( "load", function(event) {
+            alert( event.target.responseText );
+          } );
+          XHR.addEventListener( "error", function( event ) {
+            alert( 'Oops! Something went wrong.' );
+          } );
+    }
+
+    sign_up.addEventListener( "submit", function ( event ) {
+        event.preventDefault();
+    
+        sendData();
+    })
+})
+
+const url = 'https://voathclone.vercel.app/api/v1/auth/signin'
+
+let btn_ =  document.getElementById('btn-signIn')
+let login_ = document.getElementById('login_')
+
+
+window.addEventListener('load', ()=>{
+    function sendData() {
+        const XHR = new XMLHttpRequest();
+
+        const FD =  new FormData(login_)
+        XHR.open( "POST", url)
+        XHR.send(FD)
+
+
+        XHR.addEventListener( "load", function(event) {
+            alert( event.target.responseText);
+          } );
+          XHR.addEventListener( "error", function( event ) {
+            alert( 'Oops! Something went wrong.' );
+          } );
+    }
+
+
+    login_.addEventListener( "submit", function ( event ) {
+        event.preventDefault();
+    
+        sendData();
+    })
+})
+
+
+//Recommendation script
+
+fetch('https://voathclone.vercel.app/api/v1/predict', {
+    method : 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body : JSON.stringify({
+    }),
+})
+.then(
+    
+)
+
+
+
 
 
 
--- Frontend/Scripts/script2.js
+++ Frontend/Scripts/script2.js
@@ -8,6 +8,7 @@ let descrip = document.querySelector('#description')
 let temp = document.querySelector('#temp')
 let wind = document.querySelector('#wind')
 let humidity = document.querySelector('#humidity')
+let rain = document.querySelector('#rain')
 
 
 let apik = "3e2930a606c1c6880391f32a652b3ff0"
@@ -17,37 +18,39 @@ let apik = "3e2930a606c1c6880391f32a652b3ff0"
 function convertion(val){
     return (val - 273).toFixed(2)
 }
-//Now we have to collect all the information with the help of fetch method
 
+let latitude;
+let longitude;
     btn.addEventListener('click', function(){
 
-//This is the api link from where all the information will be collected
 
         fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputval.value+'&appid='+apik)
         .then(res => res.json())
 
-         //.then(data => console.log(data))
-
         .then(data => {
-            console.log(data)
-
-//Now you need to collect the necessary information with the API link. Now I will collect that information and store it in different constants.
+         
 
             let nameval = data['name']
             let descrip = data['weather']['0']['description']
             let tempature = data['main']['temp']
             let wndspd = data['wind']['speed']
             let humid =  data['main']['humidity']
-//Now with the help of innerHTML you have to make arrangements to display all the information in the webpage.
+            latitude = data['coord']['lat']
+            longitude = data['coord']['lon']
             city.innerHTML=`Weather Condition of <span>${nameval}</span>`
             temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
             description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
             wind.innerHTML = `Wind Speed: <span>${wndspd} km/h </span>`
             humidity.innerHTML = `Humudity : <span>${humid} </span>` 
-
+            // rain.innerHTML = `Rain in the last 1hr: <span>${rain_data} </span>`
         })
-
-//Now the condition must be added that what if you do not input anything in the input box.
         .catch(err => alert('You entered Wrong city name'))
     })
-//If you click on the submit button without inputting anythi
+
+btn.addEventListener('click', ()=>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apik}`)
+    .then(res => res.json())
+    .then(data => 
+        console.log(data))
+})
+
